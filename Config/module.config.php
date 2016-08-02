a16c1b2c0d4146c9d1a05ec6230dca2947fc9e6a
David Yang <daworld.ny@gmail.com>
2016-08-02 21:56:15 +0500

    Tweak for module manager

--- Config/module.config.php
+++ Config/module.config.php
@@ -3,7 +3,8 @@
 return array(
  'route' => 'Block:Admin:Block@gridAction',
  'icon'  => 'fa fa-cubes fa-5x',
- 'name'  => 'HTML Blocks',
+ 'name'  => 'Block',
+ 'caption'  => 'HTML Blocks',
  'order' => 1,
  'description' => 'HTML Blocks module allows you to dynamically handle HTML blocks'
 );
