d66f149059d0a3537732b9e2b1643120f1375ad4
David Yang <daworld.ny@gmail.com>
2017-02-04 23:09:21 +0500

    Dropped extra abstraction

--- Controller/Admin/Block.php
+++ Controller/Admin/Block.php
@@ -97,7 +97,26 @@ final class Block extends AbstractController
      */
     public function deleteAction($id)
     {
-        return $this->invokeRemoval('blockManager', $id);
+        $service = $this->getModuleService('blockManager');
+
+        // Batch removal
+        if ($this->request->hasPost('toDelete')) {
+            $ids = array_keys($this->request->getPost('toDelete'));
+
+            $service->deleteByIds($ids);
+            $this->flashBag->set('success', 'Selected elements have been removed successfully');
+
+        } else {
+            $this->flashBag->set('warning', 'You should select at least one element to remove');
+        }
+
+        // Single removal
+        if (!empty($id)) {
+            $service->deleteById($id);
+            $this->flashBag->set('success', 'Selected element has been removed successfully');
+        }
+
+        return '1';
     }
 
     /**
@@ -109,7 +128,7 @@ final class Block extends AbstractController
     {
         $input = $this->request->getPost('block');
 
-        return $this->invokeSave('blockManager', $input['id'], $input, array(
+        $formValidator = $this->createValidator(array(
             'input' => array(
                 'source' => $input,
                 'definition' => array(
@@ -118,5 +137,27 @@ final class Block extends AbstractController
                 )
             )
         ));
+
+        if ($formValidator->isValid()) {
+            $service = $this->getModuleService('blockManager');
+
+            // Update
+            if (!empty($input['id'])) {
+                if ($service->update($input)) {
+                    $this->flashBag->set('success', 'The element has been updated successfully');
+                    return '1';
+                }
+
+            } else {
+                // Create
+                if ($service->add($input)) {
+                    $this->flashBag->set('success', 'The element has been created successfully');
+                    return $service->getLastId();
+                }
+            }
+
+        } else {
+            return $formValidator->getErrors();
+        }
     }
 }
