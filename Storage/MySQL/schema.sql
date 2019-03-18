9abdca91cd3b00b9c3f742e70542ef3db5a038ce
dave-yang <daworld.ny@gmail.com>
2019-03-18 23:33:08 +0500

    Initial category relation mapper

--- Storage/MySQL/schema.sql
+++ Storage/MySQL/schema.sql
@@ -31,3 +31,24 @@ CREATE TABLE `bono_module_block_category_fields` (
 
     FOREIGN KEY (category_id) REFERENCES bono_module_block_categories(id) ON DELETE CASCADE
 ) DEFAULT CHARSET = UTF8;
+
+/* Category relation */
+DROP TABLE IF EXISTS `bono_module_block_categories_relation`;
+CREATE TABLE `bono_module_block_categories_relation` (
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Relation ID',
+    `field_id` INT NOT NULL COMMENT 'Related Field ID',
+    `page_id` INT NOT NULL COMMENT 'Page ID',
+
+    FOREIGN KEY (field_id) REFERENCES bono_module_block_category_fields(id) ON DELETE CASCADE
+
+) DEFAULT CHARSET = UTF8;
+
+DROP TABLE IF EXISTS `bono_module_block_categories_relation_translations`;
+CREATE TABLE `bono_module_block_categories_relation_translations` (
+    `id` INT NOT NULL COMMENT 'Relation ID',
+    `lang_id` INT NOT NULL COMMENT 'Attached language ID',
+    `value` TEXT NOT NULL COMMENT 'Value itself',
+
+    FOREIGN KEY (id) REFERENCES bono_module_block_categories_relation(id) ON DELETE CASCADE
+
+) DEFAULT CHARSET = UTF8;
