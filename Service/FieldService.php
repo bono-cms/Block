f6af04686032a36ccc8cf8ce5c535994431ce4f5
dave-yang <daworld.ny@gmail.com>
2019-07-13 17:55:00 +0500

    Added shared FieldService

---
+++ Service/FieldService.php
@@ -0,0 +1,99 @@
+<?php
+
+/**
+ * This file is part of the Bono CMS
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Block\Service;
+
+use Krystal\Stdlib\ArrayUtils;
+use Block\Storage\SharedFieldInterface;
+
+/**
+ * Shared service for modules that use custom fields
+ */
+final class FieldService
+{
+    /**
+     * An instance of mapper
+     * 
+     * @var \Block\Storage\SharedFieldInterface
+     */
+    private $fieldMapper;
+
+    /**
+     * State initialization
+     * 
+     * @param \Block\Storage\SharedFieldInterface $fieldMapper
+     * @return void
+     */
+    public function __construct(SharedFieldInterface $fieldMapper)
+    {
+        $this->fieldMapper = $fieldMapper;
+    }
+
+    /**
+     * Save field relation
+     * 
+     * @param int $pageId
+     * @param array $relations
+     * @return boolean
+     */
+    public function saveRelation($pageId, array $relations)
+    {
+        return $this->fieldMapper->saveRelation($pageId, $relations);
+    }
+
+    /**
+     * Save fields
+     * 
+     * @param int $pageId
+     * @param array $fields
+     * @return boolean
+     */
+    public function saveFields($pageId, array $fields)
+    {
+        // Remove previous values
+        $this->fieldMapper->deleteByColumn('page_id', $pageId);
+
+        foreach ($fields as $id => $value) {
+            $data = array(
+                'page_id' => $pageId,
+                'field_id' => $id,
+                'value' => $value
+            );
+
+            $this->fieldMapper->persist($data);
+        }
+
+        return true;
+    }
+
+    /**
+     * Returns attached fields with their values
+     * 
+     * @param int $pageId
+     * @return array
+     */
+    public function getFields($pageId)
+    {
+        return ArrayUtils::arrayPartition($this->fieldMapper->findFields($pageId), 'category');
+    }
+
+    /**
+     * Get attached category ids
+     * 
+     * @param int $pageId
+     * @param array $relations
+     * @return array
+     */
+    public function getAttachedCategories($pageId)
+    {
+        return $this->fieldMapper->findAttachedSlaves($pageId);
+    }
+}
