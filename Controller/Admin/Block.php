fecb183a1ebf0c0378db9eaa1b27a7c57af15b3f
dave-yang <daworld.ny@gmail.com>
2019-08-30 08:10:48 +0500

    Fixed issue with legacy primary key

--- Controller/Admin/Block.php
+++ Controller/Admin/Block.php
@@ -103,8 +103,8 @@ final class Block extends AbstractController
         $service = $this->getModuleService('blockManager');
 
         // Batch removal
-        if ($this->request->hasPost('toDelete')) {
-            $ids = array_keys($this->request->getPost('toDelete'));
+        if ($this->request->hasPost('batch')) {
+            $ids = array_keys($this->request->getPost('batch'));
 
             $service->deleteByIds($ids);
             $this->flashBag->set('success', 'Selected elements have been removed successfully');
